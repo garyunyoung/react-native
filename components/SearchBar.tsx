3696d9eaaea73a80255f72d25c239ed0a9157ad0
Garyun Young <garyunyoung@gmail.com>
2021-12-07 21:14:00 +1300

    Extract SearcBar to own component

---
+++ components/SearchBar.tsx
@@ -0,0 +1,104 @@
+import React from 'react'
+import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete'
+
+import { View, Alert, Text } from 'react-native'
+
+import CONSTANTS from '../variables/constants'
+import { styles } from '../styles/HomeScreenStyle'
+
+export default function SearchBar(props: any) {
+  function addAddressToList(address: any) {
+    if (props.addressess.length === 5) {
+      Alert.alert('Hello', 'Current address limit is 5', [
+        {
+          text: 'OK',
+          onPress: () => console.log('Alert Dismissed')
+        }
+      ])
+    } else if (
+      props.addressess !== [] &&
+      props.addressess.some(
+        (existingAddress) =>
+          existingAddress.key === address.key
+      )
+    ) {
+      Alert.alert(
+        'Hello',
+        'Address is already added, please select another address',
+        [
+          {
+            text: 'OK',
+            onPress: () => console.log('Alert Dismissed')
+          }
+        ]
+      )
+    } else {
+      props.setAddresses([...props.addressess, address])
+    }
+  }
+
+  function getAddressComponentValue(
+    details: any,
+    component: string
+  ) {
+    for (let addressComponent of details?.address_components) {
+      if (addressComponent.types.includes(component)) {
+        return addressComponent.long_name
+      }
+    }
+  }
+
+  function handleOnPress(details: any) {
+    let key = details?.place_id
+    let streetNumber = getAddressComponentValue(
+      details,
+      'street_number'
+    )
+    let route = getAddressComponentValue(details, 'route')
+    let sublocality = getAddressComponentValue(
+      details,
+      'sublocality'
+    )
+    let locality = getAddressComponentValue(
+      details,
+      'locality'
+    )
+
+    const address = {
+      key: key,
+      streetAddress: `${streetNumber} ${route}`,
+      city: `${sublocality} ${locality}`,
+      coordinates: {
+        latitude: details?.geometry.location.lat,
+        longitude: details?.geometry.location.lng
+      }
+    }
+
+    const region = {
+      latitude: details?.geometry.location.lat,
+      longitude: details?.geometry.location.lng,
+      latitudeDelta: 0.0922,
+      longitudeDelta: 0.0421
+    }
+
+    props.setMapRegion(region)
+    addAddressToList(address)
+  }
+
+  return (
+    <View style={styles.searchBar}>
+      <GooglePlacesAutocomplete
+        placeholder="Search"
+        fetchDetails={true}
+        onPress={(_data, details = null) =>
+          handleOnPress(details)
+        }
+        query={{
+          key: CONSTANTS.GOOGLE_PLACES_API_KEY,
+          language: 'en',
+          components: 'country:nz'
+        }}
+      />
+    </View>
+  )
+}
--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,8 +1,6 @@
 import React, { useState } from 'react'
 
-import MapViewDirections from 'react-native-maps-directions'
 import { StatusBar as ExpoStatusBar } from 'expo-status-bar'
-import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete'
 
 import {
   SafeAreaView,
@@ -13,8 +11,8 @@ import {
 } from 'react-native'
 
 import Map from '../components/Map'
+import SearchBar from '../components/SearchBar'
 
-import CONSTANTS from '../variables/constants'
 import { styles } from '../styles/HomeScreenStyle'
 
 export default function HomeScreen() {
@@ -88,123 +86,6 @@ export default function HomeScreen() {
         />
       </View>
       <ExpoStatusBar style="auto" />
-    </View>
-  )
-}
-
-function Directions(props: any) {
-  const origin = props.directions[0]
-  const waypoints = props.directions.slice(1, -1)
-
-  return (
-    <MapViewDirections
-      origin={origin}
-      waypoints={waypoints}
-      destination={origin}
-      apikey={CONSTANTS.GOOGLE_PLACES_API_KEY}
-      optimizeWaypoints={true}
-      onReady={(result) => {
-        console.log(`Distance: ${result.distance} km`)
-        console.log(`Duration: ${result.duration} min.`)
-        console.log(`Waypoint: ${result.waypointOrder}.`)
-      }}
-    />
-  )
-}
-
-function SearchBar(props: any) {
-  function addAddressToList(address: any) {
-    if (props.addressess.length === 5) {
-      Alert.alert('Hello', 'Current address limit is 5', [
-        {
-          text: 'OK',
-          onPress: () => console.log('Alert Dismissed')
-        }
-      ])
-    } else if (
-      props.addressess !== [] &&
-      props.addressess.some(
-        (existingAddress) =>
-          existingAddress.key === address.key
-      )
-    ) {
-      Alert.alert(
-        'Hello',
-        'Address is already added, please select another address',
-        [
-          {
-            text: 'OK',
-            onPress: () => console.log('Alert Dismissed')
-          }
-        ]
-      )
-    } else {
-      props.setAddresses([...props.addressess, address])
-    }
-  }
-
-  function getAddressComponentValue(
-    details: any,
-    component: string
-  ) {
-    for (let addressComponent of details?.address_components) {
-      if (addressComponent.types.includes(component)) {
-        return addressComponent.long_name
-      }
-    }
-  }
-
-  function handleOnPress(details: any) {
-    let key = details?.place_id
-    let streetNumber = getAddressComponentValue(
-      details,
-      'street_number'
-    )
-    let route = getAddressComponentValue(details, 'route')
-    let sublocality = getAddressComponentValue(
-      details,
-      'sublocality'
-    )
-    let locality = getAddressComponentValue(
-      details,
-      'locality'
-    )
-
-    const address = {
-      key: key,
-      streetAddress: `${streetNumber} ${route}`,
-      city: `${sublocality} ${locality}`,
-      coordinates: {
-        latitude: details?.geometry.location.lat,
-        longitude: details?.geometry.location.lng
-      }
-    }
-
-    const region = {
-      latitude: details?.geometry.location.lat,
-      longitude: details?.geometry.location.lng,
-      latitudeDelta: 0.0922,
-      longitudeDelta: 0.0421
-    }
-
-    props.setMapRegion(region)
-    addAddressToList(address)
-  }
-
-  return (
-    <View style={styles.searchBar}>
-      <GooglePlacesAutocomplete
-        placeholder="Search"
-        fetchDetails={true}
-        onPress={(_data, details = null) =>
-          handleOnPress(details)
-        }
-        query={{
-          key: CONSTANTS.GOOGLE_PLACES_API_KEY,
-          language: 'en',
-          components: 'country:nz'
-        }}
-      />
     </View>
   )
 }
