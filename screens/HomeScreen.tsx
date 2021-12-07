74b038ae0cb937c03a2f30cfe366993dd08f6d1d
Garyun Young <garyunyoung@gmail.com>
2021-12-07 15:23:13 +1300

    Move home style to styles folder

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -3,19 +3,15 @@ import MapView, { Marker } from 'react-native-maps'
 import { StatusBar as ExpoStatusBar } from 'expo-status-bar'
 import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete'
 
-import CONSTANTS from '../variables/constants'
-import { height, width } from '../variables/theme'
-
 import {
-  Platform,
-  StyleSheet,
   SafeAreaView,
   View,
   Text,
-  FlatList,
-  TextInput,
-  StatusBar
+  FlatList
 } from 'react-native'
+
+import CONSTANTS from '../variables/constants'
+import { styles } from '../styles/HomeScreenStyle'
 
 export default function HomeScreen() {
   const [addressess, setAddresses] = useState([])
@@ -68,7 +64,7 @@ function SearchBar(props: any) {
   ) {
     for (let addressComponent of details?.address_components) {
       if (addressComponent.types.includes(component)) {
-        return addressComponent.short_name
+        return addressComponent.long_name
       }
     }
   }
@@ -139,78 +135,3 @@ function ListItem(props: any) {
     </View>
   )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: 'white'
-  },
-
-  header: {
-    position: 'absolute',
-    top:
-      Platform.OS === CONSTANTS.ANDROID
-        ? StatusBar.currentHeight
-        : 0,
-    left: 0,
-    right: 0,
-
-    marginHorizontal: 16
-  },
-
-  searchBar: {
-    flexDirection: 'row',
-    alignItems: 'center',
-
-    backgroundColor: 'white',
-
-    paddingHorizontal: 16,
-
-    borderRadius: 50,
-    borderWidth: 1,
-    borderColor: 'lightgray',
-
-    position: 'relative'
-  },
-
-  searchBarTextInputWrapper: {
-    flex: 1
-  },
-
-  map: {
-    height: height * 0.5,
-    width: width,
-    backgroundColor: 'lightblue'
-  },
-
-  heading: {
-    paddingVertical: 16,
-    paddingHorizontal: 16,
-
-    borderBottomWidth: 2,
-    borderBottomColor: 'black'
-  },
-
-  headingText: {
-    fontSize: 24
-  },
-
-  listItem: {
-    flexDirection: 'row',
-
-    paddingVertical: 8,
-    paddingHorizontal: 16,
-
-    borderBottomWidth: 1,
-    borderBottomColor: 'black'
-  },
-
-  listItemDelete: {
-    marginRight: 16,
-    color: 'red'
-  },
-
-  listItemNumber: {
-    marginRight: 16
-  }
-})
---
+++ styles/HomeScreenStyle.tsx
@@ -0,0 +1,83 @@
+import {
+  Platform,
+  StyleSheet,
+  StatusBar
+} from 'react-native'
+
+import CONSTANTS from '../variables/constants'
+import { height, width } from '../variables/theme'
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'white'
+  },
+
+  header: {
+    position: 'absolute',
+    top:
+      Platform.OS === CONSTANTS.ANDROID
+        ? StatusBar.currentHeight
+        : 0,
+    left: 0,
+    right: 0,
+
+    marginHorizontal: 16
+  },
+
+  searchBar: {
+    flexDirection: 'row',
+    alignItems: 'center',
+
+    backgroundColor: 'white',
+
+    paddingHorizontal: 16,
+
+    borderRadius: 50,
+    borderWidth: 1,
+    borderColor: 'lightgray',
+
+    position: 'relative'
+  },
+
+  searchBarTextInputWrapper: {
+    flex: 1
+  },
+
+  map: {
+    height: height * 0.5,
+    width: width,
+    backgroundColor: 'lightblue'
+  },
+
+  heading: {
+    paddingVertical: 16,
+    paddingHorizontal: 16,
+
+    borderBottomWidth: 2,
+    borderBottomColor: 'black'
+  },
+
+  headingText: {
+    fontSize: 24
+  },
+
+  listItem: {
+    flexDirection: 'row',
+
+    paddingVertical: 8,
+    paddingHorizontal: 16,
+
+    borderBottomWidth: 1,
+    borderBottomColor: 'black'
+  },
+
+  listItemDelete: {
+    marginRight: 16,
+    color: 'red'
+  },
+
+  listItemNumber: {
+    marginRight: 16
+  }
+})
